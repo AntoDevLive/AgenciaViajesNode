0bcbf6e43c5c3b7522ff175708677ed6ed4d0e22
antoniocp99 <antoniocastizo99@gmail.com>
2025-09-22 14:09:29 +0200

    Fin del proyecto

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,18 +1,40 @@
 import { Viaje } from '../models/Viaje.js';
 import { Testimonial } from '../models/Testimoniales.js';
 
-const paginaInicio = (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio'
-    })
+//Inicio
+const paginaInicio = async (req, res) => {
+    //Consultar 3 viajes del modelo Viaje y 3 del modelo Testimonial
+
+    //Ambas consultas deben hacerse al mismo tiempo por lo que await hará que la siguiente línea no se ejecute hasta que esté lista la anterior
+
+    //Solución: hacer que ambas se ejecuten a la vez incluyendolas en un array
+    const promiseDB = [];
+    promiseDB.push(Viaje.findAll({limit: 3}))
+    promiseDB.push(Testimonial.findAll({ limit: 3 }))
+
+    try {
+        //ejecuta las dos promesas en paralelo y las muestra en el orden especificado anteriormente
+        const resultado = await Promise.all(promiseDB);
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1],
+        })
+    } catch (error) {
+        console.log(error)
+    }
 };
 
+//Nosotros
 const paginaNosotros = (req, res) => {
     res.render('nosotros', {
         pagina: 'Nosotros'
     })
 };
 
+//Viajes
 const paginaViajes = async (req, res) => {
     //Consultar BD
     const viajes = await Viaje.findAll()
@@ -23,6 +45,7 @@ const paginaViajes = async (req, res) => {
     })
 };
 
+//Testimoniales
 const paginaTestimoniales = async (req, res) => {
     try {
         const testimoniales = await Testimonial.findAll();
@@ -39,17 +62,17 @@ const paginaTestimoniales = async (req, res) => {
 
 //Muestra un viaje por su slug
 const paginaDetalleViaje = async (req, res) => {
-    const{viaje} = req.params
+    const { viaje } = req.params
 
     try {
-        const resultado = await Viaje.findOne({ where: {slug: viaje} });
+        const resultado = await Viaje.findOne({ where: { slug: viaje } });
         res.render('viaje', {
             pagina: 'Información Viaje',
             resultado
         })
     } catch (error) {
         consolr.log(error)
-    }   
+    }
 }
 
 export {
