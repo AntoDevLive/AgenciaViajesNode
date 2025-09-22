15f75cf7d719878634db7c0deb08afb845610228
antoniocp99 <antoniocastizo99@gmail.com>
2025-09-22 11:35:14 +0200

    Mostrar cada viaje por su slug

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -29,9 +29,25 @@ const paginaTestimoniales = (req, res) => {
 };
 
 
+//Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => {
+    const{viaje} = req.params
+
+    try {
+        const resultado = await Viaje.findOne({ where: {slug: viaje} });
+        res.render('viaje', {
+            pagina: 'Información Viaje',
+            resultado
+        })
+    } catch (error) {
+        consolr.log(error)
+    }   
+}
+
 export {
     paginaInicio,
     paginaNosotros,
     paginaViajes,
-    paginaTestimoniales
+    paginaTestimoniales,
+    paginaDetalleViaje
 }
